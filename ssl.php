2d454a8d4c2a19201695d9bfe3b86b42645296ba
cloud443 <121417859+cloud443@users.noreply.github.com>
2023-01-03 22:29:14 +0530

    Update ssl.php

--- ssl.php
+++ ssl.php
@@ -1,24 +1,32 @@
 <?php
 
-$domain = $_GET['domain'];
+// Set the default custom domain and the frequency of the IP check (in seconds)
+$default_domain = 'api.hifyc.link';
+$default_check_frequency = 3600; // 1 hour
 
-exec("sudo certbot --apache --redirect -n --expand -d $domain", $output, $return_var);
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // Read the domain and check frequency from the query parameters
+    $domain = isset($_GET['domain']) ? $_GET['domain'] : $default_domain;
+    $check_frequency = isset($_GET['frequency']) ? intval($_GET['frequency']) : $default_check_frequency;
 
-if ($return_var == 0) {
-  // The command was successful
-  http_response_code(200);
-  $result = array(
-    'status' => 'success',
-    'message' => "HTTPS was successfully enabled for $domain"
-  );
+    // Initialize the resolved IP address
+    $ip_address = gethostbyname($domain);
+
+    // Resolve the IP address for the domain
+    $new_ip_address = gethostbyname($domain);
+
+    // If the IP address has changed, trigger Certbot to generate a new SSL certificate
+    if ($new_ip_address != $ip_address) {
+        $ip_address = $new_ip_address;
+        exec('sudo certbot --apache --redirect -n --expand -d ' . $domain);
+    }
+
+    // Return the resolved IP address in the response
+    header('Content-Type: application/json');
+    echo json_encode(['ip_address' => $ip_address]);
 } else {
-  // The command failed
-  http_response_code(500);
-  $result = array(
-    'status' => 'error',
-    'message' => "An error occurred while trying to enable HTTPS for $domain"
-  );
+    // Return an error for unsupported request methods
+    header('Content-Type: application/json');
+    header('HTTP/1.1 405 Method Not Allowed');
+    echo json_encode(['error' => 'Method not allowed']);
 }
-
-header('Content-Type: application/json');
-echo json_encode($result);
